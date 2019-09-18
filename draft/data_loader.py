d7832d17c6679ba1d586878a3e64560f7b72244e
s3nh <steam.panek@gmail.com>
2019-09-18 16:35:33 -0400

    add ImageLoader

--- draft/data_loader.py
+++ draft/data_loader.py
@@ -17,22 +17,22 @@ def build_args():
     args = parser.parse_args()
     return args
 
-
 class ImageLoader():
     def __init__(self, image_path ):
         """
         Get Image, box, label name
         """
-        
         self.image_path = image_path
         self.image = self.load_image()
-        self.label_path = os.path.split('.')[0] + '.txt' 
+        self.label_path = self.image_path.split('.')[0] + '.txt' 
+        print(self.label_path)
+        self.label = self.load_label()
         print("Loaded image size {}".format(self.image.shape)) 
     
     def load_label(self):
         assert os.path.exists(self.label_path)
         with open(self.label_path, 'r') as labelfile:
-            _label = labelfile.readlines()
+            _label = labelfile.read()
         return _label
         
     def load_image(self):
@@ -41,6 +41,25 @@ class ImageLoader():
         return image 
     
 def main():
-    if __name__ == "__main__":
-        iml =  ImageLoader()
+    iml = ImageLoader(image_path = 'data_files/benchmarks/endtoend/eu/test_062.jpg')
+    print(iml.label_path)
+    _image = iml.image
+    #_image.show()
+    #cv2.imshow('image0', _image)
+    #cv2.waitKey(0)
+    # Name x1 x2 y1 y2
+    _label_coords = [el for el in iml.label.split()]
+    x, y = int(_label_coords[1]) ,  int(_label_coords[3])
+    height =  x - int(_label_coords[2])
+    width = y  -  int(_label_coords[4] )
+    label = _label_coords[-1]
+    _l_plate = _image[ y:(y+height), x:(x+width)]
+    _l_plate = Image.fromarray(_l_plate)
+
+
+    _l_plate.save('_test_lplate.png')
     
+    
+if __name__ == "__main__":
+    main()
+        
